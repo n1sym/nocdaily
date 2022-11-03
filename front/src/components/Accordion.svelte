7402e53e8c6a09ba35849f8741af28046e0930d8
n1sym <owlbook248@gmail.com>
2022-11-03 12:09:23 +0900

    astro

---
+++ front/src/components/Accordion.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Item from './Item.svelte'
+
+  export let desc;
+  export let keywords;
+
+	const data = {
+		'続きを読む': [desc, keywords.split("/").join("　")],
+	}
+</script>
+{#each Object.entries(data) as entry}
+<Item {entry} />
+{/each}
---
+++ front/src/components/Item.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let entry
+	let isOpen = false
+	const toggle = () => isOpen = !isOpen
+</script>
+<style>
+  .desc {line-height: 1.4;}
+  .keywords { line-height: 1.3; color: #aa7700; font-size: 14px; font-family: "Hiragino Kaku Gothic Pro", "ヒラギノ角ゴ Pro W3","ＭＳ Ｐゴシック", "MS P Gothic", "Arial", "Helvetica", "sans-serif";}
+  p {padding-left: 4px; padding-right: 4px;}
+	button {border: none; background: none; display:block; color: rgb(92, 90, 90); font-size: 16px; cursor: pointer; margin: 0; padding: 0}
+</style>
+<button on:click={toggle} aria-expanded={isOpen}> {entry[0]} </button>
+{#if isOpen}
+	<p class="desc">{entry[1][0]}</p>
+  <p class="keywords">{entry[1][1]}</p>
+{/if}
