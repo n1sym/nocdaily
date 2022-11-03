735332cec5d577f10516533146388aa95b7dffa0
n1sym <owlbook248@gmail.com>
2022-11-03 19:01:54 +0900

    UI fix

--- front/src/components/Item.svelte
+++ front/src/components/Item.svelte
@@ -5,11 +5,14 @@
 </script>
 <style>
   .desc {line-height: 1.4;}
+  .tuduki {float: left; padding-left: 4px;}
   .keywords { line-height: 1.3; color: #aa7700; font-size: 14px; font-family: "Hiragino Kaku Gothic Pro", "ヒラギノ角ゴ Pro W3","ＭＳ Ｐゴシック", "MS P Gothic", "Arial", "Helvetica", "sans-serif";}
   p {padding-left: 4px; padding-right: 4px;}
-	button {border: none; background: none; display:block; color: rgb(92, 90, 90); font-size: 16px; cursor: pointer; margin: 0; padding: 0}
+	button { width: 100%; border: none; background: rgb(234, 236, 236); display:block; color: rgb(92, 90, 90); font-size: 16px; cursor: pointer; margin: 0; padding: 0}
 </style>
-<button on:click={toggle} aria-expanded={isOpen}> {entry[0]} </button>
+<button on:click={toggle} aria-expanded={isOpen}>
+  <span class="tuduki"> {entry[0]}</span>
+</button>
 {#if isOpen}
 	<p class="desc">{entry[1][0]}</p>
   <p class="keywords">{entry[1][1]}</p>
